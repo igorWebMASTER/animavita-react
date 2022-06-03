504863e00bd0593c0964c6a36d45ad972e48ed4f
igor.sobral <igor.ribeiro@pagtel.com.br>
2022-06-02 23:19:17 -0300

    feat: add CardItem to components

---
+++ src/components/Character/CardItem/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as S from './styles'
+
+export default function CardItem({ character }: any) {
+  return (
+    <S.CardItemContainer>
+      <S.CardImage src={character.attributes?.image?.small} />
+      <S.CardCoverImg src={character.attributes?.posterImage?.large} >
+
+      </S.CardCoverImg>
+      <S.CoverContent>
+        <S.CardCharacterName>{character?.attributes?.canonicalTitle}</S.CardCharacterName>
+        <S.CardCharacterBio>{character?.attributes?.description}</S.CardCharacterBio>
+      </S.CoverContent>
+    </S.CardItemContainer>
+  )
+}
---
+++ src/components/Character/CardItem/styles.ts
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+export const CardItemContainer =  styled.div`
+  height: 50rem;
+  width: 100%;
+  display: flex;
+  position: relative;
+  background-color: white;
+  border-radius: 8px;
+  overflow: hidden;
+
+  cursor: pointer;
+
+  margin-right: 25px;
+  background-color: #fff;
+  position: relative;
+  border-radius: 12px;
+  transition: all .4s cubic-bezier(0.175, 0.885, 0, 1);
+  box-shadow: var(--shadow-elevation-medium);
+`
+
+export const CardCharacterName =  styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+export const CardCharacterBio =  styled.p`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  width: 100%;
+  text-overflow: ellipsis;
+`
+
+export const CardCharacterInfo =  styled.p`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  width: 100%;
+  text-overflow: ellipsis;
+`
+
+export const CardCoverImg =  styled.div<{ src?: string }>`
+  width: 100%;
+  background-image: url(${props => props.src || ''});
+  background-size: contain;
+  border-radius: 8px;
+  position: relative;
+  transition: all .4s cubic-bezier(0.175, 0.885, 0, 1);
+  box-shadow: var(--shadow-elevation-medium);
+
+  &:hover{
+    box-shadow: 0px 13px 10px -7px rgba(0, 0, 0,0.1);
+    transform: scale(1.10, 1.10);
+  }
+`
+
+export const CardImage =  styled.div<{ src?: string }>`
+  width: 100%;
+  height: 120px ;
+  top: 0;
+  position: absolute ;
+  background-image: url(${props => props.src || ''});
+  background-size: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+  width: 100%;
+  position: absolute;
+  height: 235px;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+  top: 0;
+`
+
+export const CoverContent =  styled.div`
+  background: #f2f2f2;
+  position: absolute;
+  width: 100%;
+  height: 20rem;
+  bottom: 0;
+  border-radius: 8px;
+  display: flex;
+  padding: 1.6rem 2rem;
+  flex-direction: column;
+  overflow-y: auto;
+  transition: all 300ms ease-in-out;
+
+  &:hover{
+    height: 100%;
+  }
+`
